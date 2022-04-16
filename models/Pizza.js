7e1630afe514ee6a3f8e079950d076d8256ad9ef
Alex Noble-James <alexnoble143.an@gmail.com>
2022-04-15 21:33:34 -0400

    finished comment model and routes

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,21 +1,42 @@
 const { Schema, model } = require("mongoose");
+const dateFormat = require('../utils/dateFormat');
 
-const Pizzaschema = new Schema({
-  pizzaName: {
-    type: String,
+const Pizzaschema = new Schema(
+  {
+    pizzaName: {
+      type: String,
+    },
+    createdBy: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+    size: {
+      type: String,
+      default: "Large",
+    },
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "comment",
+      },
+    ],
   },
-  createdBy: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  size: {
-    type: String,
-    default: "Large",
-  },
-  toppings: [],
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false,
+  }
+);
+
+Pizzaschema.virtual("commentCount").get(function () {
+  return this.comments.length;
 });
 
 const Pizza = model("Pizza", Pizzaschema);
